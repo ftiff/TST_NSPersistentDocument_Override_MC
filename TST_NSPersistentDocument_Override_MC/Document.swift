9be5d9571302149e1b552580b90901816f1da9c8
Francois 'ftiff' Levaux-Tiffreau <fti@me.com>
2017-09-17 22:28:35 +0200

    First working copy

--- TST_NSPersistentDocument_Override_MC/Document.swift
+++ TST_NSPersistentDocument_Override_MC/Document.swift
@@ -12,17 +12,36 @@ class Document: NSPersistentDocument {
 
     override init() {
         super.init()
-        // Add your subclass-specific initialization here.
+        
+        // Set managedObjectContext to mainQueue
+        // https://gist.github.com/smic/4632383
+        
+        guard let context = self.managedObjectContext else { fatalError("no MOC") }
+        if context.concurrencyType != NSManagedObjectContextConcurrencyType.mainQueueConcurrencyType {
+            guard let undoManager = context.undoManager else { fatalError("no undoManager") }
+            guard let persistentStoreCoordinator = context.persistentStoreCoordinator else { fatalError("no persistentStoreCoordinator") }
+            let newContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+            newContext.persistentStoreCoordinator = persistentStoreCoordinator
+            newContext.undoManager = undoManager
+            self.managedObjectContext = newContext
+        }
+        
+        assert(self.managedObjectContext?.concurrencyType == NSManagedObjectContextConcurrencyType.mainQueueConcurrencyType)
     }
 
     override class var autosavesInPlace: Bool {
         return true
     }
+    
+
 
     override func makeWindowControllers() {
         // Returns the Storyboard that contains your Document window.
         let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
         let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("Document Window Controller")) as! NSWindowController
+        guard let context = self.managedObjectContext else { fatalError("context is nil") }
+        guard let viewController = windowController.contentViewController  else { fatalError("cannot get contentViewcontroller") }
+        (viewController as! ViewController).mainContext = context
         self.addWindowController(windowController)
     }
 
--- TST_NSPersistentDocument_Override_MC/ViewController.swift
+++ TST_NSPersistentDocument_Override_MC/ViewController.swift
@@ -10,10 +10,44 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @objc dynamic var mainContext: NSManagedObjectContext!
+    var bgContext: NSManagedObjectContext!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+                NotificationCenter.default.addObserver(self,
+                                                       selector: #selector(ViewController.mergeChangesFromBackground(notification:)),
+                                                       name: NSNotification.Name.NSManagedObjectContextDidSave,
+                                                       object: self.bgContext)
+//                NotificationCenter.default.addObserver(self,
+//                                                       selector: #selector(ViewController.mergeChangesFromMain(notification:)),
+//                                                       name: NSNotification.Name.NSManagedObjectContextDidSave,
+//                                                       object: self.mainContext)
+    }
+    
+    @objc func mergeChangesFromBackground(notification: Notification) {
+        self.mainContext.mergeChanges(fromContextDidSave: notification)
+    }
+    
+//    @objc func mergeChangesFromMain(notification: Notification) {
+//        self.bgContext.mergeChanges(fromContextDidSave: notification)
+//    }
+    
+    override func viewDidAppear() {
+        self.bgContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        self.bgContext.parent = self.mainContext
+        
+        self.bgContext.perform {
+            let newEntity: Entity = Entity(context: self.bgContext)
+            newEntity.name = "TEST"
+            do {
+                try self.bgContext.save()
+            } catch {
+                DispatchQueue.main.async {
+                    self.presentError(error)
+                }
+            }
+        }
     }
 
     override var representedObject: Any? {
